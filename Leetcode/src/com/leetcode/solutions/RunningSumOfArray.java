ca9039737d924eb757f0e69198fdbe05198fe31e
thomas <thomas.whitetx@outlook.com>
2020-08-31 15:10:39 -0500

    Test commit for github repo.

---
+++ Leetcode/src/com/leetcode/solutions/RunningSumOfArray.java
@@ -0,0 +1,12 @@
+package com.leetcode.solutions;
+
+public class RunningSumOfArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+
+	}
+
+}
